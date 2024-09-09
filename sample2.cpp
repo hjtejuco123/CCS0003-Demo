06a8fa29f0337a2332f1ee9e687e14e6705be39e
hjtejuco123 <hadji.tejuco@gmail.com>
2024-09-09 08:34:58 +0800

    Create sample2.cpp

---
+++ sample2.cpp
@@ -0,0 +1,14 @@
+//examples of data types
+#include <iostream>
+using namespace std;
+int main(){
+	int number = 20; // integer
+	const double pi = 3.151659; //double
+	char letter = 'A';
+	bool isValid = true;
+	cout << "Value of number: "<< number << endl;
+	cout << "Value of PI "<< pi <<endl;
+	cout << "Value of Letter " << letter << endl;
+	cout << "Value of isValid " << (isValid ?"True":"False")<<endl;	
+	return 0;
+}
