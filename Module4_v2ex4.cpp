8138fbb2b7c695a19ce1d58e5c573e378a81f9fc
hjtejuco123 <hadji.tejuco@gmail.com>
2024-09-23 08:04:13 +0800

    Create Module4_v2ex4.cpp

---
+++ Module4_v2ex4.cpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Reading characters
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    char initial;
+    cout << "Enter your initial ";
+    cin >> initial;
+    
+    cout << "Your initial is " << initial <<endl;
+
+    return 0;
+}
